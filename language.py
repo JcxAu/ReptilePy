9d73c23b2d61d07a8c501fea7d84fa4616c22190
JcxAu <159135958+JcxAu@users.noreply.github.com>
2024-10-05 22:34:42 +0800

    Update language.py

--- language.py
+++ language.py
@@ -1,3 +1,16 @@
+# Copyright (c) 2024, JcxAu
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice,
+#   this list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# See the LICENSE file for more details.
 SimplifiedChinese_English = {
     '暂无': 'None',
     '开启': 'Open',
